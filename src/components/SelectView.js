7684df0f197019d8125dea20327218c7eeb4af85
César Valladares <cvalladares@ndscognitivelabs.com>
2019-11-07 19:45:43 -0600

    images

--- src/components/SelectView.js
+++ src/components/SelectView.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { action_get_travell_data,
-         action_add_option,
-         action_get_options
+import {
+  action_get_travell_data,
+  action_add_option,
+  action_get_options
 } from '../actions/actions';
 import { CssBaseline } from '@material-ui/core';
 import { Container } from '@material-ui/core';
@@ -29,102 +30,100 @@ const mapearEstado = state => ({
 
 class SelectView extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-          selectedOption: '',
-          renderList: false,
-          reciebeOption: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: '',
+      renderList: false,
+      reciebeOption: false
     }
+  }
 
-    componentDidMount(){
-        console.log(this.props);
-        const props = this.props.selectedTravell;
-        this.props.action_get_travell_data(props);
-        this.setState({
-          renderList: true
-        });
-    }
+  componentDidMount() {
+    console.log(this.props);
+    const props = this.props.selectedTravell;
+    this.props.action_get_travell_data(props);
+    this.setState({
+      renderList: true
+    });
+  }
 
-    componentDidUpdate(){
-      console.log(this.props);
-    }
+  componentDidUpdate() {
+    console.log(this.props);
+  }
 
-    selectOption(id){
-      if (!this.props.opts)
-        this.props.action_add_option(id);
-      else
-        this.props.action_add_option(id);
-      this.setState({
-        reciebeOption: true
-      });
-    }
+  selectOption(id) {
+    if (!this.props.opts)
+      this.props.action_add_option(id);
+    else
+      this.props.action_add_option(id);
+    this.setState({
+      reciebeOption: true
+    });
+  }
 
-  render(){
+  render() {
     return (
-        <React.Fragment>
-            {
-              //<p>{JSON.stringify(this.props)}</p>
-            }
-            <Typography variant="h1" style={{textAlign: 'center'}}>Destino</Typography>
-            <CssBaseline/>
-            <Container>
-                <Paper style={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent: 'space-around',
-                        overflow: 'hidden',
-                }}>
-                    <GridList cellHeight={250} style={{width: '100%', height: 500}}>
-                      {this.state.renderList && this.props.availableOptions.map(tile => (
-                        <GridListTile key={tile.id}>
-                            {
-                                //<img src={tile.img} alt={tile.title} />
-                            }
-                            afffff
-                          <GridListTileBar
-                            title={tile.title}
-                            subtitle={<span>
-                                        By: {tile.owner}
-                                        <br/>
-                                        Cost: {tile.cost}
-                                        </span>
-                                      }
-                            actionIcon={
-                              <IconButton onClick={() => this.selectOption(tile.id)} aria-label={`info about ${tile.title}`} style={{color: 'rgba(255, 255, 255, 0.54)'}}>
-                                <InfoIcon />
-                              </IconButton>
-                            }
-                          />
-                        </GridListTile>
-                      ))}
-                    </GridList>
-                </Paper>
+      <React.Fragment>
+        {
+          //<p>{JSON.stringify(this.props)}</p>
+        }
+        <Typography variant="h1" style={{ textAlign: 'center' }}>Destino</Typography>
+        <CssBaseline />
+        <Container>
+          <Paper style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+            overflow: 'hidden',
+          }}>
+            <GridList cellHeight={250} style={{ width: '100%', height: 500 }}>
+              {this.state.renderList && this.props.availableOptions.map(tile => (
+                <GridListTile key={tile.id}>
 
-                <Paper>
-                  <GridList style={{ flexWrap: 'nowrap', transform: 'translateZ(0)'}} cols={2.5}>
-                    {this.state.renderList && this.props.availableOptions.map(tile => (
-                      <GridListTile key={tile.img}>
-                        <img src={tile.img} alt={tile.title} />
-                        <GridListTileBar
-                          title={tile.title}
-                          classes={{
-                            root: {background: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'},
-                            title: {color: 'white'},
-                          }}
-                          actionIcon={
-                            <IconButton aria-label={`star ${tile.title}`}>
-                              <StarBorderIcon style={{color: 'white'}} />
-                            </IconButton>
-                          }
-                        />
-                      </GridListTile>
-                    ))}
-                  </GridList>
-                </Paper>
-            </Container>
-        </React.Fragment>
+                  <img src='https://www.entornoturistico.com/wp-content/uploads/2016/09/Productos-y-servicios-de-las-agencias-de-viajes-660x330.jpg' />
+                  <GridListTileBar
+                    title={tile.title}
+                    subtitle={<span>
+                      By: {tile.owner}
+                      <br />
+                      Cost: {tile.cost}
+                    </span>
+                    }
+                    actionIcon={
+                      <IconButton onClick={() => this.selectOption(tile.id)} aria-label={`info about ${tile.title}`} style={{ color: 'rgba(255, 255, 255, 0.54)' }}>
+                        <InfoIcon />
+                      </IconButton>
+                    }
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </Paper>
+
+          <Paper>
+            <GridList style={{ flexWrap: 'nowrap', transform: 'translateZ(0)' }} cols={2.5}>
+              {this.state.renderList && this.props.availableOptions.map(tile => (
+                <GridListTile key={tile.img}>
+                  <img src='https://image.freepik.com/vector-gratis/viajar-mundo_23-2147734014.jpg?3'/>
+                  <GridListTileBar
+                    title={tile.title}
+                    classes={{
+                      root: { background: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)' },
+                      title: { color: 'white' },
+                    }}
+                    actionIcon={
+                      <IconButton aria-label={`star ${tile.title}`}>
+                        <StarBorderIcon style={{ color: 'white' }} />
+                      </IconButton>
+                    }
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </Paper>
+        </Container>
+      </React.Fragment>
     );
   }
 }
